70fafee60643f932b44c8c7dd96fe53a008ce308
Eslam Tamim <mimatmalxe@gmail.com>
2023-11-20 16:43:24 +0200

    scoreboard updated

--- FCISQuestionsHub.Core/Models/StudentUser.cs
+++ FCISQuestionsHub.Core/Models/StudentUser.cs
@@ -7,29 +7,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
 
-namespace FCISQuestionsHub.Core.Models
+namespace FCISQuestionsHub.Core.Models;
+
+public enum Department
 {
-    public class StudentUser : IdentityUser
-    {
-        //public StudentUser() { 
-        //    UserName = Email!.Substring(0, Email.IndexOf('@'));
-        //}
-        [Required,MaxLength(50)]
-        public string FirstName { get; set; }
-        [Required, MaxLength(50)]
-        public string LastName { get; set; }
-        public string  Image { get; set; }
-        public virtual List<Question> questions { get; set; }
-        public virtual List<StudentQuestionAnswer> studentQuestions { get; set; }
-
-        
-
-
-        // SetFirstNameAsync
-
-        // SetLastNameAsync
-     
-
-
-    }
+    General, IT, CS, IS
 }
+public class StudentUser : IdentityUser
+{
+   
+    [Required,MaxLength(50)]
+    public string FirstName { get; set; }
+    [Required, MaxLength(50)]
+    public string LastName { get; set; }
+    public string Image { get; set; }
+
+    [MaxLength(50)]
+    public string  DisplayName { get; set; }
+    public Year? AcademicYear { get; set; }
+    public Department ?Department { get; set; }
+    public int ? Section { get; set; }
+
+    public virtual List<Question> questions { get; set; }
+    public virtual List<StudentQuestionAnswer> studentQuestions { get; set; }
+
+    
+
+
+    // SetFirstNameAsync
+
+    // SetLastNameAsync
+ 
+
+
+}
+
---
+++ FCISQuestionsHub.EF/Migrations/20231027181223_MoreInfoToUser.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCISQuestionsHub.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoreInfoToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AcademicYear",
+                schema: "security",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Department",
+                schema: "security",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                schema: "security",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Section",
+                schema: "security",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcademicYear",
+                schema: "security",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Department",
+                schema: "security",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                schema: "security",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Section",
+                schema: "security",
+                table: "Users");
+        }
+    }
+}
